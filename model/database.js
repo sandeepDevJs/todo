4d786940cfbbd6e9f92078567850c6cb09c799d3
Sandeep Gupta <sandeepguptabc@gmail.com>
2020-11-23 21:39:06 +0530

    comment

--- model/database.js
+++ model/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //mongo atlas connection
 module.exports = function(){
 
-    mongoose.connect("mongodb+srv://test:test@cluster0.8fnai.mongodb.net/Cluster0?retryWrites=true&w=majority")
+    mongoose.connect("ADD MONGODB CONNECTION STRING HERE!!!")
         .then(() => console.log("Connected To Db!!"))
         .catch((err) => console.log(err));
 
